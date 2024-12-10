86dbb4aad814d7f692c4b15939f0cba4af39c6ea
hazyplebian <michael.mangieri@yahoo.com>
2024-12-10 18:08:28 -0500

    <Tidied up toggle for light/dark them>

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const themeToggle = document.getElementById('btn-check');
+const toggleLabel = document.getElementById('toggle-label');
 
 // Check the user's saved theme preference (if any)
 const savedTheme = localStorage.getItem('theme');
 if (savedTheme) {
     document.documentElement.setAttribute('data-theme', savedTheme);
-    themeToggle.textContent = savedTheme === 'dark' ? 'Switch to Light Mode' : 'Switch to Dark Mode';
+    toggleLabel.textContent = savedTheme === 'dark' ? 'Switch to Light Mode' : 'Switch to Dark Mode';
 }
 
 // Toggle theme and update local storage
@@ -13,7 +14,7 @@ themeToggle.addEventListener('click', () => {
     const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
     document.documentElement.setAttribute('data-theme', newTheme);
     localStorage.setItem('theme', newTheme);
-    themeToggle.textContent = newTheme === 'dark' ? 'Switch to Light Mode' : 'Switch to Dark Mode';
+    toggleLabel.textContent = newTheme === 'dark' ? 'Switch to Light Mode' : 'Switch to Dark Mode';
 });
 
 //confirms selection of intensity and target area from dropdown menu and saves selection to local storage
